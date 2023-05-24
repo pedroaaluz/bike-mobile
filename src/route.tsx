6cacde172f47765acf3e32e2e038dcc6061346ab
pedroaaluz <pedroaluz137@gmail.com>
2023-05-23 21:04:50 -0300

    install paper and navigation

---
+++ src/route.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Map from './screen/Map';
+import type {TabParamsList} from './types/rootStackParamListType';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+
+const Tab = createBottomTabNavigator<TabParamsList>();
+
+export const Route = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        {/*    <Tab.Screen name="Nap" component={Map} /> */}
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
---
+++ src/types/rootStackParamListType.ts
@@ -0,0 +1,3 @@
+export type TabParamsList = {
+  Map: undefined;
+};
